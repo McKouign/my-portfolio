50bb0fc7c03aa3a9545bfba16db92f79ed243e31
Romain Le Neindre <romain.le.neindre@gmail.com>
2024-01-23 16:43:41 +0100

    banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import { NavBar } from "./Components/Navbar";
+import { Banner } from "./Components/Banner";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
