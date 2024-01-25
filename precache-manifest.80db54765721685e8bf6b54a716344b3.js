2ec857a8f72c63c4bcb675932db6c71e96f1b705
Romain Le Neindre <romain.le.neindre@gmail.com>
2024-01-25 14:25:45 +0100

    Updates

---
+++ precache-manifest.80db54765721685e8bf6b54a716344b3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7fdee8a68ce56698a8728c0dc56101",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "a9e5bf7282680a6d5602",
+    "url": "/my-portfolio/static/css/2.5f0330fc.chunk.css"
+  },
+  {
+    "revision": "9392c13aa0ba98c76ce9",
+    "url": "/my-portfolio/static/css/main.a8363753.chunk.css"
+  },
+  {
+    "revision": "a9e5bf7282680a6d5602",
+    "url": "/my-portfolio/static/js/2.1eb22591.chunk.js"
+  },
+  {
+    "revision": "89d43ad9a4e13acb6b29fb12d3eb4028",
+    "url": "/my-portfolio/static/js/2.1eb22591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87db0bb15081e41bb271",
+    "url": "/my-portfolio/static/js/3.0aa9c7f2.chunk.js"
+  },
+  {
+    "revision": "9392c13aa0ba98c76ce9",
+    "url": "/my-portfolio/static/js/main.b76ea231.chunk.js"
+  },
+  {
+    "revision": "05ca603b42897f371c16",
+    "url": "/my-portfolio/static/js/runtime-main.92157baa.js"
+  },
+  {
+    "revision": "baa70c4449cb420453ba0f22c5eb80aa",
+    "url": "/my-portfolio/static/media/Profile-pic.baa70c44.jpg"
+  },
+  {
+    "revision": "fa455fef320fa62a48fc9e977203d65a",
+    "url": "/my-portfolio/static/media/bf-pic.fa455fef.jpeg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/my-portfolio/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/my-portfolio/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/my-portfolio/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "48380c60710c937d337c7c669a7e5787",
+    "url": "/my-portfolio/static/media/github.48380c60.png"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/my-portfolio/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "5fe164df167a1c1820e3d598be342509",
+    "url": "/my-portfolio/static/media/instagram.5fe164df.png"
+  },
+  {
+    "revision": "bcf463ab5e1550ec1c859b0624d05715",
+    "url": "/my-portfolio/static/media/linkedin.bcf463ab.png"
+  },
+  {
+    "revision": "72c5d146bc83ff2e110b623af36e9476",
+    "url": "/my-portfolio/static/media/pdc-pic.72c5d146.jpeg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/my-portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/my-portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/my-portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "18ce6348ed65d5c51680bc8fab87a226",
+    "url": "/my-portfolio/static/media/togotosleep.18ce6348.jpg"
+  }
+]);
