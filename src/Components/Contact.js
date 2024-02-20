67c3befe155a21fe7591f04f39c419dc1f9f4d84
Romain Le Neindre <romain.le.neindre@gmail.com>
2024-02-20 15:13:33 +0100

    contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ContactImg from "../Assets/images/contact-img.svg";
+import navIcon1 from "../Assets/images/linkedin.png";
+import navIcon2 from "../Assets/images/github.png";
+import navIcon3 from "../Assets/images/instagram.png";
 
 export const Contact = () => {
   return (
@@ -11,13 +14,25 @@ export const Contact = () => {
           <div className="contact-image">
             <img src={ContactImg} alt="Contact me" />
           </div>
+          
           <div className="contact-info">
+            <div className="social-icon mb-3">
+                <a href="https://www.linkedin.com/in/romainln/" target="_blank">
+                  <img src={navIcon1} alt="" />
+                </a>
+                <a href="https://github.com/McKouign" target="_blank">
+                  <img src={navIcon2} alt="" />
+                </a>
+                <a href="https://www.instagram.com/mckouign/" target="_blank">
+                  <img src={navIcon3} alt="" />
+                </a>
+              </div>
             <h2>Mes coordonnées</h2>
             <p>
               <strong>Email :</strong> romain.le.neindre@gmail.com
             </p>
             <p>
-              <strong>Téléphone :</strong> 06 10 42 56 27
+              <strong>Tel :</strong> 06 10 42 56 27
             </p>
           </div>
         </div>
